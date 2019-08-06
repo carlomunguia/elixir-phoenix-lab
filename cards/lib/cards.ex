80127c55b1927450ca2510960232583d967cc245
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-06 16:13:39 -0500

    pattern matching matching patterns pattern matching matching patterns

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -19,4 +19,18 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
+  end
 end
