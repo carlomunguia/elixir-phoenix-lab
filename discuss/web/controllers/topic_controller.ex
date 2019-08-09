1eb7dad96ac7cd03f626861706f1e2042fea87bf
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-08 23:54:11 -0500

    ohai changeset

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,10 +1,14 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
-  def new(conn, params) do
+  alias Discuss.Topic
 
+  def new(conn, params) do
+    changeset = Topic.changeset(%Topic{}, %{})
   end
 end
+
+
 
 
 
