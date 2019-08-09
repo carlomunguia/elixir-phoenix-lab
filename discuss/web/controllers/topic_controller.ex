733a0f593ba3edb1b8044693ebc886df9f31eb44
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-08 23:07:12 -0500

    working on the topic controller/model and now moving into form creation

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,15 +2,9 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   def new(conn, params) do
-    IO.puts "++++"
-    IO.inspect conn
-    IO.puts "++++"
-    IO.inspect params
-    IO.puts "++++"
+
   end
 end
-
-
 
 
 
