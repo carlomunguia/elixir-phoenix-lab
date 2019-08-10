2a079848b063a7ebc592faf23f6760f3ef7b9800
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-10 12:53:55 -0500

    topic funcitonality complete!

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -4,7 +4,8 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   def index(conn, _params) do
-
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
   end
 
   def new(conn, _params) do
