80e58df4be172d1dba8192b9be2aea9e7328174c
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-15 21:48:52 -0500

    finished the topic controller!

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -34,8 +34,9 @@ defmodule Discuss.TopicController do
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
-    changeset = Repo.get(Topic, topic_id)
-                |> Topic.changeset(topic)
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
 
     case Repo.update(changeset) do
       {:ok, _topic} ->
@@ -43,8 +44,17 @@ defmodule Discuss.TopicController do
         |> put_flash(:info, "Topic Updated")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
-        render conn, "edit.html", changeset: changeset
+        render conn, "edit.html", changeset: changeset, topic: old_topic
     end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id)
+    |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
   end
 end
 
